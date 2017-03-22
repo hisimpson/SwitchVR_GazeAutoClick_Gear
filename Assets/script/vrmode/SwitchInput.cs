cccece4993c5f7e8b894dfba9600c5e101f50593
wow <wow@daum-naver.com>
2017-03-23 08:52:11 +0900

    기어 VR에서 UGUI 데모

---
+++ Assets/script/vrmode/SwitchInput.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SwitchInput {
+
+    Transform gvrControllerMainTrans;
+    Transform gvrControllerPointerTrans;
+    Transform gvrGazePointerTrans;
+    GvrPointerInputModule gvrPointerInputModule;
+    CustomGvrPointerInputModule customGvrPointerInputModule;
+
+    //GvrLaserPointer의 근본적인 문제가 아니다. 
+    //Cardboard 관련 input과 Daydream 관련 input을 모두 비활성화 시켜 놓고 실행해야 
+    //Cardboard와 Daydream input이 충돌 하지 않는다.
+
+    public void Init(GameObject eventSystem, GameObject gvrControllerMain)
+    {
+        gvrControllerMainTrans = gvrControllerMain.transform;
+        Transform cameraTrans = Camera.main.gameObject.transform;
+        gvrControllerPointerTrans = cameraTrans.parent.Find("GvrControllerPointer");
+        gvrGazePointerTrans = cameraTrans.Find("CustomReticlePointer");
+
+        CheckNull.LogError("gvrGazePointerTrans is null", gvrGazePointerTrans);
+        CheckNull.LogError("gvrControllerPointerTrans is null", gvrControllerPointerTrans);
+
+        Transform laserTrans = gvrControllerPointerTrans.transform.Find("Laser");
+
+        gvrPointerInputModule = eventSystem.GetComponent<GvrPointerInputModule>();
+        customGvrPointerInputModule = eventSystem.GetComponent<CustomGvrPointerInputModule>();
+
+        CheckNull.LogError("gvrPointerInputModule is null", gvrPointerInputModule);
+        CheckNull.LogError("customGvrPointerInputModule is null", customGvrPointerInputModule);
+    }
+	
+    public void ChangeMode(SwitchVRMode.UseVrDevice useVrDevice)
+    {
+        gvrControllerMainTrans.gameObject.SetActive(useVrDevice == SwitchVRMode.UseVrDevice.DAYDREAM);
+
+        GameObject[] objects = null;
+        try
+        {
+            objects = GameObject.FindGameObjectsWithTag("ROOT");
+        }
+        catch (System.Exception exception)
+        {
+            Debug.Log(exception);
+        }
+
+        for (int n = 0; n < objects.Length; ++n)
+        {
+            GameObject obj = objects[n];
+
+            //TODO: 레이어 이름 가져오기
+            //http://tikuma1181.com/HPmain/?p=865
+            //if (LayerMask.LayerToName(obj.layer) != "UI")
+            if (obj.layer != LayerMask.NameToLayer("UI"))
+                continue;
+
+            if (useVrDevice == SwitchVRMode.UseVrDevice.CARDBOARD)
+                SetCardboardUI(obj);
+            else if (useVrDevice == SwitchVRMode.UseVrDevice.DAYDREAM)
+                SetDaydreamUI(obj);
+        }
+
+        SetCardboardCursor(useVrDevice == SwitchVRMode.UseVrDevice.CARDBOARD);
+
+        customGvrPointerInputModule.enabled = (useVrDevice == SwitchVRMode.UseVrDevice.CARDBOARD);
+        gvrPointerInputModule.enabled = (useVrDevice == SwitchVRMode.UseVrDevice.DAYDREAM);
+    }
+
+    void SetDaydreamUI(GameObject obj)
+    {
+        GvrPointerGraphicRaycaster gvrRaycaster = obj.GetComponent<GvrPointerGraphicRaycaster>();
+        GraphicRaycaster graphicRaycaster = obj.GetComponent<GraphicRaycaster>();
+
+        CheckNull.LogError("gvrRaycaster is null", gvrRaycaster);
+        CheckNull.LogError("graphicRaycaster is null", graphicRaycaster);
+
+        gvrRaycaster.enabled = true;
+        graphicRaycaster.enabled = false;
+    }
+
+    void SetCardboardUI(GameObject obj)
+    {
+        GvrPointerGraphicRaycaster gvrRaycaster = obj.GetComponent<GvrPointerGraphicRaycaster>();
+        GraphicRaycaster graphicRaycaster = obj.GetComponent<GraphicRaycaster>();
+
+        CheckNull.LogError("gvrRaycaster is null", gvrRaycaster);
+        CheckNull.LogError("graphicRaycaster is null", graphicRaycaster);
+
+        gvrRaycaster.enabled = true;
+        graphicRaycaster.enabled = false;
+    }
+
+    void SetCardboardCursor(bool isGaze)
+    {
+        gvrGazePointerTrans.gameObject.SetActive(isGaze);
+        gvrControllerPointerTrans.gameObject.SetActive(!isGaze);
+    }
+}
---
+++ Assets/script/vrmode/SwitchVRMode.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using UnityEngine.UI;
+
+public class SwitchVRMode : MonoBehaviour
+{
+    float nextTransition = 5.0f;
+
+    float transitionTime;
+    public enum Mode { NOVR, VR, MAX };
+    public Mode mode { get; private set; }
+
+
+    public enum UseVrDevice { NONE, CARDBOARD, DAYDREAM };
+    public UseVrDevice useVrDevice { get; private set;  }
+
+    void Awake()
+    {
+        //daydream과 cardboard를 에디터에서 테스트 할려면 여기만 설정하면 된다.
+#if UNITY_EDITOR
+        mode = Mode.VR;
+        useVrDevice = UseVrDevice.CARDBOARD;
+        //useVrDevice = UseVrDevice.DAYDREAM;
+#endif
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        transitionTime = Time.timeSinceLevelLoad + nextTransition;
+        //yield return StartCoroutine(ChangeMode(mode));
+        
+        string[] supportedDevices = VRSettings.supportedDevices;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void DetectMode()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (string.IsNullOrEmpty(VRDevice.model) == true)
+                mode = Mode.NOVR;
+            else
+            {
+                mode = Mode.VR;
+                if (VRDevice.model.IndexOf("daydream", StringComparison.CurrentCultureIgnoreCase) > -1)
+                    useVrDevice = UseVrDevice.DAYDREAM;
+                else
+                    useVrDevice = UseVrDevice.CARDBOARD;
+            }
+        }
+    }
+
+    public IEnumerator ChangeDeviceMode(Mode emode)
+    {
+        if (mode == Mode.NOVR)
+        {
+            Debug.Log("Change Mode: none Device");
+            VRSettings.LoadDeviceByName("none");
+        }
+        else if (mode == Mode.VR)
+        {
+            Debug.Log("Change Mode: VR Device");
+            //If failed loading daydream device, load "cardboard" device.
+            VRSettings.LoadDeviceByName("daydream");
+            //VRSettings.LoadDeviceByName("cardboard");
+        }
+
+        yield return null;
+
+#if UNITY_EDITOR
+        if (mode == Mode.VR)
+            GvrViewer.Instance.VRModeEnabled = true;
+        else
+            GvrViewer.Instance.VRModeEnabled = false;
+#else
+        if (mode == Mode.VR)
+        {
+            VRSettings.enabled = true;
+            if (VRDevice.model.IndexOf("daydream", StringComparison.CurrentCultureIgnoreCase) > -1)
+            {
+                Debug.Log("Change Mode: VR daydream Device");
+                useVrDevice = UseVrDevice.DAYDREAM;
+            }   
+            else
+            {
+                Debug.Log("Change Mode: VR cardboard Device");
+                useVrDevice = UseVrDevice.CARDBOARD;
+            }
+        }
+#endif
+    }
+}
